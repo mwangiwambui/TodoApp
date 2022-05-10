199564c2250d669a6db6ae43c3892858b0c5ee9e
mwangiwambui <wambui54mwangi@gmail.com>
2022-05-10 20:00:37 +0300

    Changed from browser router to react router

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
 <TodoContainer />
-</BrowserRouter>
+</HashRouter>
 </React.StrictMode>, document.getElementById("root"))
